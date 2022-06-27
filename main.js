954f1c6f2444ae868b073218239e7cd0b8cefc7f
Sidharth Ponram <76591884+sidharthponram916@users.noreply.github.com>
2022-06-27 08:52:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+const Discord = require('discord.js');
+const client = new Discord.Client(); 
+
+const k = "OTA1NTA1MDc5ODcwNzU4OTc1.YYLDUg."
+const e = "EcsDLCGQ0LtT"
+const y = "_zJZW_sWEZvyW_E"
+
+let channelName = ""; 
+let name = "VaderAI"; 
+const chalk = require('chalk');
+
+const readline = require('readline'); 
+const rl = readline.createInterface({ 
+   input: process.stdin,
+   output: process.stdout, 
+   terminal: false
+})
+
+client.on('ready', async () => { 
+
+   client.user.setActivity("im online lmao");
+
+     console.log(chalk.blueBright(`Discord FeatherWeight Edition V1.0.2`)); 
+     console.log(chalk.redBright(`Powered by ${client.user.tag}`)); 
+
+
+         
+    await rl.question(`Enter the name of the channel you want to access: `, async data => { 
+        try { 
+            channelName = data;
+
+            /*  let last_messages = await client.channels.cache.get(`${channel_id}`).messages.fetch({ limit: 30}); */
+            let channel = client.channels.cache.find(c => c.name === `${channelName}`);
+ 
+            let last_messages = await channel.messages.fetch({ limit: 30}); 
+
+            last_messages = Array.from(last_messages.values()).reverse();  
+   
+            for (i = 0; i < 30; i++) { 
+               let msg = last_messages[i]; 
+      
+                console.log(`${chalk.yellowBright(msg.author.username)}: ${msg.content}`); 
+            }
+   
+            channel.send(`**${name}** has connected to this channel via Discord FeatherWeight`)
+         }
+         catch (e) { 
+            console.log(e); 
+         }
+      })
+
+})   
+   
+
+client.on('message', async msg => { 
+
+      if (msg.channel.name == channelName) {            
+           if (msg.author.tag !== "VaderAI#7202") { 
+               console.log(`${chalk.blueBright(`${msg.author.username}`)}: ${msg.content}`);
+           }   
+      }
+
+     await rl.question(chalk.magenta('') , async message => { 
+         if (message.trim('') !== '') {
+            if (message.trim('').startsWith(">switch")) {
+                channelName = message.trim('').split(' ')[1];
+
+                let channel = client.channels.cache.find(c => c.name === `${channelName}`);
+
+                msg.channel.send(`**${name}** has disconnected from the channel. `); 
+
+
+
+                let last_messages = await channel.messages.fetch({ limit: 30}); 
+       
+                last_messages = Array.from(last_messages.values()).reverse();  
+       
+                for (i = 0; i < 30; i++) { 
+                   let msg = last_messages[i]; 
+           
+                    console.log(`${chalk.yellowBright(msg.author.username)}: ${msg.content}`); 
+                }
+       
+                channel.send(`**${name}** has connected to this channel via DiscordFeatherweight`)
+
+               }
+            else {
+             msg.channel.send(`${message.trim('')}`)
+            }
+         }
+      })
+})
+  
+client.login(`${k}${e}${y}`);
